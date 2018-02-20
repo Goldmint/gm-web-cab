6a2d3dd6c15a7b4c0180119ec3c808c305ca1ed7
vxg <vxg>
2018-02-20 23:07:49 +0300

    -

--- DAL/Migrations/20180220194843_banned-countries-fix.Designer.cs
+++ DAL/Migrations/20180220194843_banned-countries-fix.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Goldmint.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180220194843_banned-countries-fix")]
+    partial class bannedcountriesfix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DAL/Migrations/20180220194843_banned-countries-fix.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Goldmint.DAL.Migrations
+{
+    public partial class bannedcountriesfix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "gm_signed_document",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "comment",
+                table: "gm_banned_country",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "gm_signed_document");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "comment",
+                table: "gm_banned_country",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+        }
+    }
+}
