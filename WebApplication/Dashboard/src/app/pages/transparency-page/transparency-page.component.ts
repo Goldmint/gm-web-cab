7138ad435dfbb8e6416b96e72c694d1db52a58eb
Igor Kurilo <igor.kurilo31@gmail.com>
2018-03-26 19:56:56 +0300

    dashboard for transparency

--- WebApplication/Dashboard/src/app/pages/transparency-page/transparency-page.component.ts
+++ WebApplication/Dashboard/src/app/pages/transparency-page/transparency-page.component.ts
@@ -9,6 +9,7 @@ import { TransparencyRecord } from '../../interfaces';
 import { APIService, UserService, MessageBoxService } from '../../services';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import { BigNumber } from 'bignumber.js';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -22,12 +23,14 @@ export class TransparencyPageComponent implements OnInit {
   public locale: string;
   public loading: boolean;
   public page = new Page();
+  public isDataLoaded = false;
 
   public rows:  Array<TransparencyRecord> = [];
   public sorts: Array<any> = [{prop: 'date', dir: 'desc'}];
   public messages:    any  = {emptyMessage: 'Loading...'};
 
   public form: FormGroup;
+  public statData: object;
   private amount = null;
 
   @ViewChild('file') selectedFile;
@@ -39,7 +42,8 @@ export class TransparencyPageComponent implements OnInit {
     private cdRef: ChangeDetectorRef,
     public translate: TranslateService,
     private formBuilder: FormBuilder,
-    private _messageBox: MessageBoxService
+    private _messageBox: MessageBoxService,
+    private datePipe: DatePipe
     ) {
 
     this.page.pageNumber = 0;
@@ -79,17 +83,22 @@ export class TransparencyPageComponent implements OnInit {
       .subscribe(
         data => {
           this.rows = data.data.items;
+          this.statData = data.data.stat;
+
+          this.statData['viewDataTimestamp'] = this.datePipe.transform(this.statData['dataTimestamp'] * 1000, 'dd.MM.yy');
+          this.statData['viewAuditTimestamp'] = this.datePipe.transform(this.statData['auditTimestamp'] * 1000, 'dd.MM.yy');
 
           this.page.totalElements = data.data.total;
           this.page.totalPages = Math.ceil(this.page.totalElements / this.page.size);
 
           this.loading = false;
-          this.cdRef.detectChanges();
 
           const tableTitle = document.getElementById('pageSectionTitle');
           if (tableTitle && tableTitle.getBoundingClientRect().top < 0) {
             tableTitle.scrollIntoView();
           }
+          this.isDataLoaded = true;
+          this.cdRef.detectChanges();
         });
   }
 
@@ -104,6 +113,21 @@ export class TransparencyPageComponent implements OnInit {
   onChangeFile(event) {
     let file = event.target.files[0];
     this.form.controls['file'].setValue(file ? file.name : '');
+  }
+
+  updateStat() {
+    this.loading = true;
+
+    let dataTimestamp = this.statData['viewDataTimestamp'].split('.');
+    let auditTimestamp = this.statData['viewAuditTimestamp'].split('.');
+
+    this.statData['dataTimestamp'] = Math.ceil(Date.UTC(+dataTimestamp[2] + 2000, dataTimestamp[1], dataTimestamp[0]) / 1000);
+    this.statData['auditTimestamp'] = Math.ceil(Date.UTC(+auditTimestamp[2] + 2000, auditTimestamp[1], auditTimestamp[0]) / 1000);
+
+    this.apiService.updateStatTransparency(this.statData).subscribe(() => {
+      this._messageBox.alert('Changes saved');
+      this.loading = false;
+    });
   }
 
   upload() {
