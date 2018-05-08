ecea87b1e3ae27f5b7fd3f524abd7dd215faa038
Igor Kurilo <igor.kurilo31@gmail.com>
2018-05-08 13:26:49 +0300

    added popup after send transaction for buy/sell page

--- WebApplication/Client/src/environments/environment.apidev.ts
+++ WebApplication/Client/src/environments/environment.apidev.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api/v1',
   recaptchaSiteKey: '6LcuSTcUAAAAAGGcHJdRqDN1fEmtKjYue_872F0k',
+  etherscanUrl: 'https://rinkeby.etherscan.io/tx/',
   infuraUrl: 'https://rinkeby.infura.io/ErpvjHXqcahfBLahHPfh',
   etherscanGetABIUrl: 'https://api-rinkeby.etherscan.io',
   EthContractAddress: '0x35da662961866d74941be3c682bd8db4cf324119'
