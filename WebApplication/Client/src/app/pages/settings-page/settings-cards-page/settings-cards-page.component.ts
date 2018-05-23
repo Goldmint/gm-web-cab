2eb8b22b2ef1be2cc0ec3b296322499304d4d73a
Igor Kurilo <igor.kurilo31@gmail.com>
2018-05-24 00:25:07 +0300

    fixes in verification page. Call profile method

--- WebApplication/Client/src/app/pages/settings-page/settings-cards-page/settings-cards-page.component.ts
+++ WebApplication/Client/src/app/pages/settings-page/settings-cards-page/settings-cards-page.component.ts
@@ -6,6 +6,7 @@ import { zip } from 'rxjs/observable/zip';
 import { User, CardsList, CardsListItem, APIResponse, CardStatusResponse } from '../../../interfaces';
 import { UserService, APIService, MessageBoxService } from '../../../services';
 import {TranslateService} from "@ngx-translate/core";
+import {Observable} from "rxjs/Observable";
 
 enum Page { List, OnNew, OnNeedConfirmation, OnNeedVerification, OnVerified, OnFailure }
 
@@ -48,17 +49,16 @@ export class SettingsCardsPageComponent implements OnInit {
   }
 
   ngOnInit() {
-    this._userService.currentUser
-      .subscribe(user => {
-        this.user = user;
-        this.onLoading();
-      });
+    Observable.combineLatest(
+      this._apiService.getFiatCards(),
+      this._apiService.getProfile()
+    ).subscribe((res) => {
+      this.cards = res[0].data;
+      this.user = res[1].data;
 
-    this._apiService.getFiatCards()
-      .subscribe(cards => {
-        this.cards = cards.data;
-        this.onLoading();
-      });
+      this.onLoading();
+      this._cdRef.markForCheck();
+    });
   }
 
   onLoading() {
@@ -97,7 +97,7 @@ export class SettingsCardsPageComponent implements OnInit {
   }
 
   removeCard(card) {
-    this._translate.get('MessageBox.Remove').subscribe(phrase => {
+    this._translate.get('MessageBox.Cards.Remove').subscribe(phrase => {
       this._messageBox.confirm(phrase).subscribe(ok => {
         if (ok) {
           this.processing = true;
@@ -108,11 +108,13 @@ export class SettingsCardsPageComponent implements OnInit {
             }).subscribe(() => {
             const id = this.cards.list.indexOf(card);
             id >= 0 && this.cards.list.splice(id, 1);
-            this._translate.get('MessageBox.Removed').subscribe(phrase => {
+            this._translate.get('MessageBox.Cards.Removed').subscribe(phrase => {
               this._messageBox.alert(phrase);
             });
           }, err => {
-              this._messageBox.alert(err.error.errorDesc);
+            this._translate.get('MessageBox.SomethingWrong').subscribe(phrase => {
+              this._messageBox.alert(phrase);
+            });
             }
           );
         }
