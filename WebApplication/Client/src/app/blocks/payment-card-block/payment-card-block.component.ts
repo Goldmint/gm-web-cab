a5161fc3ab3a92b04cbbac35cfb9375cfe6ba81a
Igor Kurilo <igor.kurilo31@gmail.com>
2018-05-24 15:08:41 +0300

    translation and responsive for payment card block

--- WebApplication/Client/src/app/blocks/payment-card-block/payment-card-block.component.ts
+++ WebApplication/Client/src/app/blocks/payment-card-block/payment-card-block.component.ts
@@ -1,22 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
 import {MessageBoxService} from "../../services";
 
 @Component({
   selector: 'app-payment-card-block',
   templateUrl: './payment-card-block.component.html',
-  styleUrls: ['./payment-card-block.component.sass']
+  styleUrls: ['./payment-card-block.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PaymentCardBlockComponent implements OnInit {
 
   public agreeCheck: boolean = false;
+  public isMobile: boolean = false;
 
   @Input('amount') estimatedAmount
 
   constructor(
-    private _messageBox: MessageBoxService
+    private _messageBox: MessageBoxService,
+    private cdRef: ChangeDetectorRef
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.isMobile = (window.innerWidth <= 767);
+    window.onresize = () => {
+      this.isMobile = window.innerWidth <= 767 ? true : false;
+      this.cdRef.markForCheck();
+    };
+  }
 
   onSubmit() {
     this._messageBox.alert('Coming soon');
