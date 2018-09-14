792edfc043b850ac4824812d8a4afacff8c50ada
Igor Kurilo <igor.kurilo31@gmail.com>
2018-09-14 15:30:17 +0300

    proxy infura link has been added

--- WebApplication/Client/src/environments/environment.prod.ts
+++ WebApplication/Client/src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment = {
   gasPriceLink: 'https://www.etherchain.org/api/gasPriceOracle',
   recaptchaSiteKey: '6LcuSTcUAAAAAGGcHJdRqDN1fEmtKjYue_872F0k',
   etherscanUrl: 'https://etherscan.io/tx/',
-  infuraUrl: 'https://mainnet.infura.io/ErpvjHXqcahfBLahHPfh',
+  infuraUrl: 'https://service.goldmint.io/proxy/infura/mainnet',
   etherscanGetABIUrl: 'https://api.etherscan.io',
   EthContractAddress: '0x1d9be4a5681374c54b1821d0beac3a678ad128a4'
 };
--- WebApplication/Client/src/environments/environment.stage.ts
+++ WebApplication/Client/src/environments/environment.stage.ts
@@ -9,7 +9,7 @@ export const environment = {
   gasPriceLink: 'https://www.etherchain.org/api/gasPriceOracle',
   recaptchaSiteKey: '6LcuSTcUAAAAAGGcHJdRqDN1fEmtKjYue_872F0k',
   etherscanUrl: 'https://rinkeby.etherscan.io/tx/',
-  infuraUrl: 'https://rinkeby.infura.io/ErpvjHXqcahfBLahHPfh',
+  infuraUrl: 'https://service.goldmint.io/proxy/infura/rinkeby',
   etherscanGetABIUrl: 'https://api-rinkeby.etherscan.io',
   EthContractAddress: '0x8B3Cc4BCf7cb3727565Bb69cb23Ae7D810988Df2'
 };
