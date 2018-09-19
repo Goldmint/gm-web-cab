447fe2a80f2faa72c8f8c4efcd3ce0a01b908d37
unknown <d-karasovsky@yandex.ru>
2018-09-19 16:03:55 +0300

    add promocodes migration

---
+++ DAL/Migrations/20180919130208_promo-codes-update.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Goldmint.DAL.Migrations
+{
+    public partial class promocodesupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "value",
+                table: "gm_promo_code");
+
+            migrationBuilder.AddColumn<long>(
+                name: "discount_value",
+                table: "gm_promo_code",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "limit",
+                table: "gm_promo_code",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "token_type",
+                table: "gm_promo_code",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "discount_value",
+                table: "gm_promo_code");
+
+            migrationBuilder.DropColumn(
+                name: "limit",
+                table: "gm_promo_code");
+
+            migrationBuilder.DropColumn(
+                name: "token_type",
+                table: "gm_promo_code");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "value",
+                table: "gm_promo_code",
+                type: "decimal(38, 18)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
