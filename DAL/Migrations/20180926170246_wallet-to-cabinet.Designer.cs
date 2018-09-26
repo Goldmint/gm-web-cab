cdf11fbde2de9417afb568fe605fc378ed5f9b1c
unknown <d-karasovsky@yandex.ru>
2018-09-26 20:18:12 +0300

    add wallet db migration

--- DAL/Migrations/20180926170246_wallet-to-cabinet.Designer.cs
+++ DAL/Migrations/20180926170246_wallet-to-cabinet.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Goldmint.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180926170246_wallet-to-cabinet")]
+    partial class wallettocabinet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
