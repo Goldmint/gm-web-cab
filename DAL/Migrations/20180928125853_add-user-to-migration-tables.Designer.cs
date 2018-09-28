f37e0b0d00e2db255b9a63ace75582df196bdd3e
unknown <d-karasovsky@yandex.ru>
2018-09-28 16:01:51 +0300

    add user field in migration tables

--- DAL/Migrations/20180928125853_add-user-to-migration-tables.Designer.cs
+++ DAL/Migrations/20180928125853_add-user-to-migration-tables.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Goldmint.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180928125853_add-user-to-migration-tables")]
+    partial class addusertomigrationtables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DAL/Migrations/20180928125853_add-user-to-migration-tables.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Goldmint.DAL.Migrations
+{
+    public partial class addusertomigrationtables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "user_id",
+                table: "gm_se_token_migration",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "user_id",
+                table: "gm_es_token_migration",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_gm_se_token_migration_user_id",
+                table: "gm_se_token_migration",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_gm_es_token_migration_user_id",
+                table: "gm_es_token_migration",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_gm_es_token_migration_gm_user_user_id",
+                table: "gm_es_token_migration",
+                column: "user_id",
+                principalTable: "gm_user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_gm_se_token_migration_gm_user_user_id",
+                table: "gm_se_token_migration",
+                column: "user_id",
+                principalTable: "gm_user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_gm_es_token_migration_gm_user_user_id",
+                table: "gm_es_token_migration");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_gm_se_token_migration_gm_user_user_id",
+                table: "gm_se_token_migration");
+
+            migrationBuilder.DropIndex(
+                name: "IX_gm_se_token_migration_user_id",
+                table: "gm_se_token_migration");
+
+            migrationBuilder.DropIndex(
+                name: "IX_gm_es_token_migration_user_id",
+                table: "gm_es_token_migration");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "gm_se_token_migration");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "gm_es_token_migration");
+        }
+    }
+}
