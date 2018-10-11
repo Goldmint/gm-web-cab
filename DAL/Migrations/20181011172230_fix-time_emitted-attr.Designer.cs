93690868860fc7017a86539ffb4fe1b6c915996c
unknown <d-karasovsky@yandex.ru>
2018-10-11 20:24:58 +0300

    fix time_emitted attribute

--- DAL/Migrations/20181011172230_fix-time_emitted-attr.Designer.cs
+++ DAL/Migrations/20181011172230_fix-time_emitted-attr.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Goldmint.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181011172230_fix-time_emitted-attr")]
+    partial class fixtime_emittedattr
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DAL/Migrations/20181011172230_fix-time_emitted-attr.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Goldmint.DAL.Migrations
+{
+    public partial class fixtime_emittedattr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "time_emitted",
+                table: "gm_es_token_migration",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "time_emitted",
+                table: "gm_es_token_migration",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
