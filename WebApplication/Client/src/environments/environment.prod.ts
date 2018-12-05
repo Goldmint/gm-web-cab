05da613c86222de507bcd5a8ca15a85756d04d39
Vasiliy R <vixpalg@gmail.com>
2018-12-05 16:09:03 +0300

    contract address changed

--- WebApplication/Client/src/environments/environment.prod.ts
+++ WebApplication/Client/src/environments/environment.prod.ts
@@ -10,15 +10,15 @@ export const environment = {
   //   MainNet: 'https://service.goldmint.io/sumus/mainnet/v1',
   //   TestNet: 'https://service.goldmint.io/sumus/testnet/v1'
   // },
-  sumusNetworkUrl: 'https://service.goldmint.io/sumus/mainnet/v1',
+  sumusNetworkUrl: 'https://service.goldmint.io/sumus/testnet/v1',
   apiUrl: 'https://app.goldmint.io/api/v1',
-  walletApiUrl: 'https://app.goldmint.io/wallet/api/v1',
-  marketApiUrl: 'https://app.goldmint.io/market/v1',
+  walletApiUrl: 'https://staging.goldmint.io/wallet/api/v1',
+  marketApiUrl: 'https://staging.goldmint.io/market/v1',
   gasPriceLink: 'https://www.etherchain.org/api/gasPriceOracle',
   recaptchaSiteKey: '6LcuSTcUAAAAAGGcHJdRqDN1fEmtKjYue_872F0k',
   etherscanUrl: 'https://etherscan.io/tx/',
   infuraUrl: 'https://service.goldmint.io/proxy/infura/mainnet',
   etherscanGetABIUrl: 'https://api.etherscan.io',
-  EthContractAddress: '0x1d9be4a5681374c54b1821d0beac3a678ad128a4',
+  EthContractAddress: '0xa5dc5b5046003fa379ac6430675b543fcb69f101',
   getLiteWalletLink: 'https://chrome.google.com/webstore/detail/goldmint-lite-wallet/fnabdmcgpkkjjegokfcnfbpneacddpfh'
 };
