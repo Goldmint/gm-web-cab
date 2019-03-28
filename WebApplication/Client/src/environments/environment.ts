9d8ae80f6b113fff0c2b565e227cf11e0be56c9f
Vasiliy R <vixpalg@gmail.com>
2019-03-28 15:19:50 +0300

    rinkeby pool contract changed

--- WebApplication/Client/src/environments/environment.ts
+++ WebApplication/Client/src/environments/environment.ts
@@ -23,7 +23,7 @@ export const environment = {
   EthContractAddress: '0x30c695f0db4e63e287e5c2a567bb7ad4bca6da94',
   EthGoldContractAddress: '0xd67a3c707f901c510724703f150b1f2d94dc5ee6',
   EthMntpContractAddress: '0x160350f317b573f477473dd74c3bdfcf5e619da0',
-  EthPoolContractAddress: '0x5ef007a0bb40b4a11523ecd6a77d6bef76596b85',
+  EthPoolContractAddress: '0x3B9eD4d623983dD0544A5B7080A00C0ffb0303d2',
   EthOldPoolContractAddress: '0xcD4aE63c113a29757C2afBe240B740324A339DC2',
   getLiteWalletLink: 'https://chrome.google.com/webstore/detail/goldmint-lite-wallet/fnabdmcgpkkjjegokfcnfbpneacddpfh'
 };
