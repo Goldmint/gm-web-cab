467654b3b7b06f10a524a997c26113d4ce62f4c0
Vasiliy R <vixpalg@gmail.com>
2019-05-08 23:23:22 +0300

    pool contract address changed

--- WebApplication/Client/src/environments/environment.prod.ts
+++ WebApplication/Client/src/environments/environment.prod.ts
@@ -23,7 +23,7 @@ export const environment = {
   EthContractAddress: '0xa5dc5b5046003fa379ac6430675b543fcb69f101',
   EthGoldContractAddress: '0x61d40B844ea5B68c9C504FCcdB05B68c2D7aE965',
   EthMntpContractAddress: '0x83cee9e086A77e492eE0bB93C2B0437aD6fdECCc',
-  EthPoolContractAddress: '0xb129ef53bef95c3e42bb296145fa310d6b447ef4',
+  EthPoolContractAddress: '0x9568C8C783f7166A9b88d0047ad28EFC43921242',
   EthOldPoolContractAddress: '0x02ad0e74f0e2e4ce093aa7517901ac32f0abd370',
   getLiteWalletLink: {
     chrome: 'https://chrome.google.com/webstore/detail/goldmint-lite-wallet/fnabdmcgpkkjjegokfcnfbpneacddpfh',
