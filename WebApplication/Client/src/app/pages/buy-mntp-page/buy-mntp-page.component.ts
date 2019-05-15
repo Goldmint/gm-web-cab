58a6409a70a93e13c03d4fa7906c6828f5b3af32
Igor Kurilo <igor.kurilo31@gmail.com>
2019-05-15 18:19:00 +0300

    added coinmarketcap widget and other small corrections

--- WebApplication/Client/src/app/pages/buy-mntp-page/buy-mntp-page.component.ts
+++ WebApplication/Client/src/app/pages/buy-mntp-page/buy-mntp-page.component.ts
@@ -12,6 +12,20 @@ export class BuyMntpPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const scriptUrls = [
+      'https://files.coinmarketcap.com/static/widget/currency.js',
+      'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'
+    ];
+
+    const scripts: any = document.head.querySelectorAll('script');
+    scripts && scripts.forEach(script => {
+      scriptUrls.indexOf(script.src) >= 0 && script.parentNode.removeChild(script);
+    });
+
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = scriptUrls[0];
+    document.head.appendChild(script);
   }
 
 }
