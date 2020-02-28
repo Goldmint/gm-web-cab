7720371d3a6e404477fff85d6f6efeeedd5fb13a
Vasiliy R <vixpalg@gmail.com>
2020-02-28 16:37:16 +0300

    mnt swap prod contract updated

--- WebApplication/Client/src/environments/environment.prod.ts
+++ WebApplication/Client/src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
   etherscanApiKey: 'XUBQCPBH8T4NXB7ANE2JJI8NSF9FK12V5W',
   EthMntpContractAddress: '0x83cee9e086A77e492eE0bB93C2B0437aD6fdECCc',
   EthPoolContractAddress: '0x9568C8C783f7166A9b88d0047ad28EFC43921242',
-  SwapContractAddress: '0xfA649a1dEfc2c0673f8e97ED4d06B28078DdF12d',
+  SwapContractAddress: '0xa276a093e53168C8699D4C8f0274A7eCa71CC694',
   getLiteWalletLink: {
     chrome: 'https://chrome.google.com/webstore/detail/goldmint-lite-wallet/fnabdmcgpkkjjegokfcnfbpneacddpfh',
     firefox: 'https://addons.mozilla.org/ru/firefox/addon/goldmint-lite-wallet/'
